271b34254931c5a8b00a909e7d33bdcb40c73c07
Adam Chwilkowski <adam.chwilkowski@roche.com>
2017-09-10 22:53:09 +0200

    updated cli code; changed optimization order

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,15 @@
 #! /usr/bin/env node
 
 'use strict';
-const chalk = require('chalk');
 const argv = require('yargs').argv;
-const shell = require('shelljs');
+const exec = require('child_process').exec
+const ora = require('ora');
+const spinner = !argv.quiet && ora('create-svg-sprite..').start()
+const svgSprite = 'svg-sprite@1.3.7'
+const svgo = 'svgo@0.7.2'
 
 /**
- * Deconstruct CLI args using yargs package
+ * Deconstruct args using yargs
  * @see https://www.npmjs.com/package/yargs
  */
 const {
@@ -18,19 +21,21 @@ const {
 } = argv;
 
 /**
- * If optimize arg is availavle then run svgo compressor
- * @see https://github.com/svg/svgo
- */
-if (optimize) {
-  !argv.quiet && console.log(chalk.grey('Optimizing files..'));
-  shell.exec(`node ./node_modules/svgo/bin/svgo -f ${input} --quiet`);
-}
-
-/**
  * Create SVG sprite using svg-sprite package
  * @see https://github.com/jkphl/svg-sprite
  */
-!argv.quiet && console.log(chalk.grey('Creating sprite..'));
-shell.exec(`node ./node_modules/svg-sprite/bin/svg-sprite.js --symbol --inline --ss=\"${name}\" --symbol-dest=\"\" --dest=${output} ${input}/*.svg`)
+spinner && Object.assign(spinner, { text: 'Creating SVG sprite..' })
+exec(`npx --quiet ${svgSprite} --symbol --inline --ss=\"${name}\" --symbol-dest=\"\" --dest=${output} ${input}/*.svg`, () => {
+  spinner && spinner.succeed(`SVG sprite "${name}.svg" has been created in "${output}" based on files from "${input}" using ${svgSprite}`)
 
-!argv.quiet && console.log(chalk.blue(`✌️ SVG sprite "${name}.svg" created in "${output}" based on files from "${input}"`));
+  /**
+   * If optimize flag is available then run svgo compressor
+   * @see https://github.com/svg/svgo
+   */
+  if (optimize) {
+    spinner && spinner.start() && Object.assign(spinner, { text: 'Optimizing files..' })
+    exec(`npx --quiet ${svgo} -f ${output}/${name}.svg --quiet`, () => {
+      spinner && spinner.succeed(`SVG sprite has been optimized using ${svgo}`)
+    });
+  }
+});
